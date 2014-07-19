778c9d69c6d67d1d07ff232225b2a4f1ff824710
Eric J. Holmes <eric@ejholmes.net>
2014-07-19 09:47:01 -0700

    Cleanup.

--- buble_test.go
+++ buble_test.go
@@ -13,27 +13,41 @@ type User struct {
 
 func Test_Handler(t *testing.T) {
 	tests := []struct {
-		fn          HandlerFunc
-		status      int
-		body        string
-		contentType string
+		fn       HandlerFunc
+		expected struct {
+			status      int
+			body        string
+			contentType string
+		}
 	}{
 		{
 			fn: HandlerFunc(func(resp *Response, req *Request) {
 				resp.SetStatus(200)
 				resp.Present(&User{ID: 1})
 			}),
-			status:      200,
-			body:        `{"id":1}` + "\n",
-			contentType: "application/json",
+			expected: struct {
+				status      int
+				body        string
+				contentType string
+			}{
+				status:      200,
+				body:        `{"id":1}` + "\n",
+				contentType: "application/json",
+			},
 		},
 		{
 			fn: HandlerFunc(func(resp *Response, req *Request) {
 				resp.SetStatus(200)
 			}),
-			status:      200,
-			body:        `{}` + "\n",
-			contentType: "application/json",
+			expected: struct {
+				status      int
+				body        string
+				contentType string
+			}{
+				status:      200,
+				body:        `{}` + "\n",
+				contentType: "application/json",
+			},
 		},
 	}
 
@@ -44,17 +58,17 @@ func Test_Handler(t *testing.T) {
 		h := &Handler{HandlerFunc: test.fn}
 		h.ServeHTTP(resp, req)
 
-		if resp.Code != test.status {
-			t.Errorf("Status: Want %v, Got %v", test.status, resp.Code)
+		if resp.Code != test.expected.status {
+			t.Errorf("Status: Want %v, Got %v", test.expected.status, resp.Code)
 		}
 
-		if resp.Body.String() != test.body {
-			t.Errorf("Body: Want %v, Got %v", test.body, resp.Body.String())
+		if resp.Body.String() != test.expected.body {
+			t.Errorf("Body: Want %v, Got %v", test.expected.body, resp.Body.String())
 		}
 
 		contentType := resp.HeaderMap.Get("Content-Type")
-		if contentType != test.contentType {
-			t.Errorf("Content-Type: Want %v, Got %v", test.contentType, contentType)
+		if contentType != test.expected.contentType {
+			t.Errorf("Content-Type: Want %v, Got %v", test.expected.contentType, contentType)
 		}
 	}
 }
